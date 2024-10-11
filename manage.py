b248cf9ce6e919f7b31c744d14f690b988c8c5e2
gongahkia <angryapplegravy@gmail.com>
2024-10-11 17:39:55 +0800

    added eveerything from jn but renamed shit

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'where_2_eat.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'what_2_eat.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
