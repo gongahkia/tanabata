085e454285fac40af0dedcd9dc11ed819070fdc2
gongahkia <angryapplegravy@gmail.com>
2024-10-14 11:43:44 +0800

    ion orchrad scraper is up

--- napkin.md
+++ napkin.md
@@ -9,8 +9,10 @@
 
 * do I want to expose the API script to make `tako` an API wrapper that exposes food places in colleges and malls? if so add that in the README.md and learn how to write a wrapper for an API
     * API is available here http://127.0.0.1:8000/api/food-places/?format=json
+    * reformat the final API for each location's food being scraped to be consistent, then define it in another file called API.md
 
 * need to debug why the script.js and style.css in my ./food/static/food/ folder is not visible when i serve my index.html file
+
 
 * Debug capitaland_spider.py code since currently my spider is unable to scrape any items
 * Debug capitaland_extractor_soup.py code since currently my bs4 scraper is unable to scrape any items
--- scrapers/shakespeare_2_eat/ion_orchard_shakespeare.py
+++ scrapers/shakespeare_2_eat/ion_orchard_shakespeare.py
@@ -5,8 +5,8 @@ from playwright.sync_api import sync_playwright
 
 def delete_file(target_url):
     """
-    Helper function that attempts to 
-    delete a file at the specified url.
+    helper function that attempts to 
+    delete a file at the specified url
     """
     try:
         os.remove(target_url)
@@ -16,7 +16,7 @@ def delete_file(target_url):
 
 def clean_string(input_string):
     """
-    Sanitizes a provided string.
+    sanitizes a provided string
     """
     cleaned_string = re.sub(r'\n+', ' ', input_string)
     cleaned_string = re.sub(r'<[^>]+>', '', cleaned_string)
@@ -24,9 +24,9 @@ def clean_string(input_string):
 
 def scrape_ion_orchard(base_urls):
     """
-    Scrapes the specified Ion Orchard 
+    scrapes the specified Ion Orchard 
     websites for the food vendor's name,
-    location, description, category, and URL.
+    location, description, category, and URL
     """
     details_list = []
     errors = []
@@ -35,35 +35,38 @@ def scrape_ion_orchard(base_urls):
         page = browser.new_page()
         for base_url in base_urls:
             try:
+
+                # ----- extraction logic ----- 
+
                 print(f"trying to scrape url: {base_url}")
                 page.goto(base_url)
                 page.wait_for_selector('div.cmp-dynamic-list-dine-shop-item-content-info')
                 print("page 1 found")
                 while True:
-                    # listings = page.query_selector_all('div.cmp-dynamic-list-dine-shop-item-content-info')
-                    # if not listings:
-                    #     errors.append("No more listings found.")
-                    #     break
-                    # for listing in listings:
-
-                    #     print(listing.inner_text())
-
-                        # name = listing.query_selector('div.cmp-dynamic-list-dine-shop-item-content-info span.cmp-dynamic-list-dine-shop-item-content-item-title').inner_text()
-                        # # raw_location = listing.query_selector('div.cmp-dynamic-list-dine-shop-item-content-info div.cmp-dynamic-list-dine-shop-item-content-info span.cmp-dynamic-list-dine-shop-item-content-item-num').inner_text().strip()
+                    listings = page.query_selector_all('div.cmp-dynamic-list-dine-shop-item-content-info')
+                    if not listings:
+                        errors.append("No more listings found.")
+                        break
+                    for listing in listings:
+                        # print(listing.inner_text())
+                        name = listing.query_selector('span.cmp-dynamic-list-dine-shop-item-content-item-title').inner_text().strip()
+                        location = listing.query_selector('span.cmp-dynamic-list-dine-shop-item-content-item-num').inner_text().strip()
                         # print(name)
-                        # # print(raw_location)
-                        # # clean_location = clean_string(raw_location.inner_text().strip() if raw_location else '')
-                        # description = "" 
-                        # category = ""
-                        # vendor_url = base_url 
-                        # details = {
-                        #     'name': name,
-                        #     'location': clean_location,
-                        #     'description': description,
-                        #     'category': category,
-                        #     'url': vendor_url
-                        # }
-                        # details_list.append(details)
+                        # print(location)
+                        description = "" 
+                        category = ""
+                        vendor_url = base_url 
+                        details = {
+                            'name': name,
+                            'location': location,
+                            'description': description,
+                            'category': category,
+                            'url': vendor_url
+                        }
+                        details_list.append(details)
+
+                    # ----- next page -----
+
                     next_page_button = page.query_selector('div.cmp-dynamic-list-pagination-container span.cmp-dynamic-list-paginate-item.active + span.cmp-dynamic-list-paginate-item')
                     if next_page_button and next_page_button.inner_text().strip() != "":
                         print(f"page {next_page_button.inner_text()} found")
